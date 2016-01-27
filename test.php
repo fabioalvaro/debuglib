31970870a7f6e6209521dbaa5bf309eecbf36a4a
fabioalvaro <fabio.alvaro@gmail.com>
2016-01-27 19:50:08 -0200

    pequenos ajustes

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+  
+ */
+
+//require_once 'vendor/debuglib.php';
+
+
+
+print_a($_SERVER);
